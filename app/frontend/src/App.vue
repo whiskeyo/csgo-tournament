6c2635df2f2957bcbf21df404fdf994bf4009dbd
whiskeyo <duchowe50k@gmail.com>
2022-01-14 06:11:57 +0100

    Add Bootstrap css to Vue app

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <Navbar/>
+  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="CS:GO Tournament web application"/>
 </template>
 
 <script>
+import 'bootstrap'
+
 import HelloWorld from './components/HelloWorld.vue'
 import Navbar from './components/Navbar.vue'
 
@@ -26,4 +28,6 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+@import'~bootstrap/dist/css/bootstrap.css'
 </style>
