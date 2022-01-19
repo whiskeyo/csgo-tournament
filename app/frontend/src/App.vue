35ea4fec74b34f7b30559adf0f26e6942b5b4a0a
whiskeyo <duchowe50k@gmail.com>
2022-01-19 15:46:53 +0100

    Remake vue dependencies

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -1,31 +1,43 @@
 <template>
+  <Navbar/>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="CS:GO Tournament web application"/>
+  <Footer/>
 </template>
 
 <script>
 import 'bootstrap'
 
+import Navbar from './components/common/Navbar.vue'
+import Footer from './components/common/Footer.vue'
+
 import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
+    Navbar,
+    Footer,
     HelloWorld,
   }
 }
 </script>
 
 <style>
+@import '~bootstrap/dist/css/bootstrap.min.css';
+@import './assets/css/styles.css';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #eee;
-  margin-top: 60px;
+  color: var(--main-text-color);
+  background-color: var(--main-bg-color);
+  margin-bottom: 50px;
+  padding-bottom: 10px;
+  min-height: 100%;
 }
 
-@import '~bootstrap/dist/css/bootstrap.min.css';
-@import './assets/css/styles.css';
 </style>
---
+++ app/frontend/src/components/common/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">CS:GO Tournament App</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+nav {
+  margin-bottom: 15px;
+}
+</style>
