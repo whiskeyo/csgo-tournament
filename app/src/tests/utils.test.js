59153d6906fbe855d894daef1a16d64cefe04af3
whiskeyo <duchowe50k@gmail.com>
2022-06-20 21:43:31 +0200

    Finish UTs for utils.js, coverage of all tested files is 100%

--- app/src/tests/utils.test.js
+++ app/src/tests/utils.test.js
@@ -105,7 +105,6 @@ describe("utils tests", () => {
     expect(utils.isItemInArray("x", array)).toBe(false);
   });
 
-
   test("calculateNextMatchIndex successfully counts indexes for 2 teams", () => {
     const numberOfTeams = 2;
     expect(utils.calculateNextMatchIndex(0, numberOfTeams)).toBe(-1);
@@ -148,6 +147,196 @@ describe("utils tests", () => {
     expect(utils.calculateNextMatchIndex(14, numberOfTeams)).toBe(-1);
   });
 
+  const maps = [
+    { name: "Map 0" },
+    { name: "Map 1" },
+    { name: "Map 2" },
+    { name: "Map 3" },
+    { name: "Map 4" },
+    { name: "Map 5" },
+    { name: "Map 6" },
+  ];
+
+  const mapsBO1 = Object.create(maps);
+  const matchDetailsBO1 = {
+    matchType: types.MatchType.BO1,
+    actionsTakenOnMaps: 0,
+    allMaps: mapsBO1,
+    mapsBanned: [],
+    maps: [],
+    scores: [],
+  };
+
+  test("setMapState moves maps to picked and banned correctly and creates score objects for BO1 matches", () => {
+    utils.setMapState(maps[3], matchDetailsBO1);
+    expect(matchDetailsBO1.maps).toStrictEqual([]);
+    expect(matchDetailsBO1.mapsBanned).toStrictEqual([maps[3].name]);
+
+    matchDetailsBO1.actionsTakenOnMaps = 1;
+    utils.setMapState(maps[2], matchDetailsBO1);
+    expect(matchDetailsBO1.maps).toStrictEqual([]);
+    expect(matchDetailsBO1.mapsBanned).toStrictEqual([maps[3].name, maps[2].name]);
+
+    matchDetailsBO1.actionsTakenOnMaps = 2;
+    utils.setMapState(maps[6], matchDetailsBO1);
+    expect(matchDetailsBO1.maps).toStrictEqual([]);
+    expect(matchDetailsBO1.mapsBanned).toStrictEqual([maps[3].name, maps[2].name, maps[6].name]);
+
+    matchDetailsBO1.actionsTakenOnMaps = 3;
+    utils.setMapState(maps[5], matchDetailsBO1);
+    expect(matchDetailsBO1.maps).toStrictEqual([]);
+    expect(matchDetailsBO1.mapsBanned).toStrictEqual([maps[3].name, maps[2].name, maps[6].name, maps[5].name]);
+
+    matchDetailsBO1.actionsTakenOnMaps = 4;
+    utils.setMapState(maps[4], matchDetailsBO1);
+    expect(matchDetailsBO1.maps).toStrictEqual([]);
+    expect(matchDetailsBO1.mapsBanned).toStrictEqual([
+      maps[3].name,
+      maps[2].name,
+      maps[6].name,
+      maps[5].name,
+      maps[4].name,
+    ]);
+
+    matchDetailsBO1.actionsTakenOnMaps = 5;
+    utils.setMapState(maps[0], matchDetailsBO1);
+    expect(matchDetailsBO1.maps).toStrictEqual([]);
+    expect(matchDetailsBO1.mapsBanned).toStrictEqual([
+      maps[3].name,
+      maps[2].name,
+      maps[6].name,
+      maps[5].name,
+      maps[4].name,
+      maps[0].name,
+    ]);
+
+    matchDetailsBO1.actionsTakenOnMaps = 6;
+    utils.setMapState(maps[1], matchDetailsBO1);
+    expect(matchDetailsBO1.maps).toStrictEqual([maps[1].name]);
+    expect(matchDetailsBO1.mapsBanned).toStrictEqual([
+      maps[3].name,
+      maps[2].name,
+      maps[6].name,
+      maps[5].name,
+      maps[4].name,
+      maps[0].name,
+    ]);
+
+    matchDetailsBO1.actionsTakenOnMaps = 100; /* example out of scope going to default */
+    utils.setMapState(maps[1], matchDetailsBO1);
+    expect(matchDetailsBO1.maps).toStrictEqual([maps[1].name]);
+    expect(matchDetailsBO1.mapsBanned).toStrictEqual([
+      maps[3].name,
+      maps[2].name,
+      maps[6].name,
+      maps[5].name,
+      maps[4].name,
+      maps[0].name,
+    ]);
+  });
+
+  const mapsBO3 = Object.create(maps);
+  const matchDetailsBO3 = {
+    matchType: types.MatchType.BO3,
+    actionsTakenOnMaps: 0,
+    allMaps: mapsBO3,
+    mapsBanned: [],
+    maps: [],
+    scores: [],
+  };
+
+  test("setMapState moves maps to picked and banned correctly and creates score objects for BO3 matches", () => {
+    utils.setMapState(maps[3], matchDetailsBO3);
+    expect(matchDetailsBO3.maps).toStrictEqual([]);
+    expect(matchDetailsBO3.mapsBanned).toStrictEqual([maps[3].name]);
+
+    matchDetailsBO3.actionsTakenOnMaps = 1;
+    utils.setMapState(maps[2], matchDetailsBO3);
+    expect(matchDetailsBO3.maps).toStrictEqual([]);
+    expect(matchDetailsBO3.mapsBanned).toStrictEqual([maps[3].name, maps[2].name]);
+
+    matchDetailsBO3.actionsTakenOnMaps = 2;
+    utils.setMapState(maps[6], matchDetailsBO3);
+    expect(matchDetailsBO3.maps).toStrictEqual([maps[6].name]);
+    expect(matchDetailsBO3.mapsBanned).toStrictEqual([maps[3].name, maps[2].name]);
+
+    matchDetailsBO3.actionsTakenOnMaps = 3;
+    utils.setMapState(maps[5], matchDetailsBO3);
+    expect(matchDetailsBO3.maps).toStrictEqual([maps[6].name, maps[5].name]);
+    expect(matchDetailsBO3.mapsBanned).toStrictEqual([maps[3].name, maps[2].name]);
+
+    matchDetailsBO3.actionsTakenOnMaps = 4;
+    utils.setMapState(maps[4], matchDetailsBO3);
+    expect(matchDetailsBO3.maps).toStrictEqual([maps[6].name, maps[5].name]);
+    expect(matchDetailsBO3.mapsBanned).toStrictEqual([maps[3].name, maps[2].name, maps[4].name]);
+
+    matchDetailsBO3.actionsTakenOnMaps = 5;
+    utils.setMapState(maps[0], matchDetailsBO3);
+    expect(matchDetailsBO3.maps).toStrictEqual([maps[6].name, maps[5].name]);
+    expect(matchDetailsBO3.mapsBanned).toStrictEqual([maps[3].name, maps[2].name, maps[4].name, maps[0].name]);
+
+    matchDetailsBO3.actionsTakenOnMaps = 6;
+    utils.setMapState(maps[1], matchDetailsBO3);
+    expect(matchDetailsBO3.maps).toStrictEqual([maps[6].name, maps[5].name, maps[1].name]);
+    expect(matchDetailsBO3.mapsBanned).toStrictEqual([maps[3].name, maps[2].name, maps[4].name, maps[0].name]);
+
+    matchDetailsBO3.actionsTakenOnMaps = 100; /* example out of scope going to default */
+    utils.setMapState(maps[1], matchDetailsBO3);
+    expect(matchDetailsBO3.maps).toStrictEqual([maps[6].name, maps[5].name, maps[1].name]);
+    expect(matchDetailsBO3.mapsBanned).toStrictEqual([maps[3].name, maps[2].name, maps[4].name, maps[0].name]);
+  });
+
+  const mapsBO5 = Object.create(maps);
+  const matchDetailsBO5 = {
+    matchType: types.MatchType.BO5,
+    actionsTakenOnMaps: 0,
+    allMaps: mapsBO5,
+    mapsBanned: [],
+    maps: [],
+    scores: [],
+  };
+
+  test("setMapState moves maps to picked and banned correctly and creates score objects for BO5 matches", () => {
+    utils.setMapState(maps[3], matchDetailsBO5);
+    expect(matchDetailsBO5.maps).toStrictEqual([]);
+    expect(matchDetailsBO5.mapsBanned).toStrictEqual([maps[3].name]);
+
+    matchDetailsBO5.actionsTakenOnMaps = 1;
+    utils.setMapState(maps[2], matchDetailsBO5);
+    expect(matchDetailsBO5.maps).toStrictEqual([]);
+    expect(matchDetailsBO5.mapsBanned).toStrictEqual([maps[3].name, maps[2].name]);
+
+    matchDetailsBO5.actionsTakenOnMaps = 2;
+    utils.setMapState(maps[6], matchDetailsBO5);
+    expect(matchDetailsBO5.maps).toStrictEqual([maps[6].name]);
+    expect(matchDetailsBO5.mapsBanned).toStrictEqual([maps[3].name, maps[2].name]);
+
+    matchDetailsBO5.actionsTakenOnMaps = 3;
+    utils.setMapState(maps[5], matchDetailsBO5);
+    expect(matchDetailsBO5.maps).toStrictEqual([maps[6].name, maps[5].name]);
+    expect(matchDetailsBO5.mapsBanned).toStrictEqual([maps[3].name, maps[2].name]);
+
+    matchDetailsBO5.actionsTakenOnMaps = 4;
+    utils.setMapState(maps[4], matchDetailsBO5);
+    expect(matchDetailsBO5.maps).toStrictEqual([maps[6].name, maps[5].name, maps[4].name]);
+    expect(matchDetailsBO5.mapsBanned).toStrictEqual([maps[3].name, maps[2].name]);
+
+    matchDetailsBO5.actionsTakenOnMaps = 5;
+    utils.setMapState(maps[0], matchDetailsBO5);
+    expect(matchDetailsBO5.maps).toStrictEqual([maps[6].name, maps[5].name, maps[4].name, maps[0].name]);
+    expect(matchDetailsBO5.mapsBanned).toStrictEqual([maps[3].name, maps[2].name]);
+
+    matchDetailsBO5.actionsTakenOnMaps = 6;
+    utils.setMapState(maps[1], matchDetailsBO5);
+    expect(matchDetailsBO5.maps).toStrictEqual([maps[6].name, maps[5].name, maps[4].name, maps[0].name, maps[1].name]);
+    expect(matchDetailsBO5.mapsBanned).toStrictEqual([maps[3].name, maps[2].name]);
+
+    matchDetailsBO5.actionsTakenOnMaps = 100; /* example out of scope going to default */
+    utils.setMapState(maps[1], matchDetailsBO5);
+    expect(matchDetailsBO5.maps).toStrictEqual([maps[6].name, maps[5].name, maps[4].name, maps[0].name, maps[1].name]);
+    expect(matchDetailsBO5.mapsBanned).toStrictEqual([maps[3].name, maps[2].name]);
+  });
+
   const banning = "Banning maps";
   const picking = "Picking maps";
   test("setBanningAndPickingPhase works properly for a BO1 match", () => {
@@ -178,5 +367,174 @@ describe("utils tests", () => {
     expect(utils.setBanningAndPickingPhase(4, types.MatchType.BO5)).toBe(picking);
     expect(utils.setBanningAndPickingPhase(5, types.MatchType.BO5)).toBe(picking);
     expect(utils.setBanningAndPickingPhase(6, types.MatchType.BO5)).toBe("");
+  });
+
+  test("canFirstCaptainTakeActionOnMap returns true if it is turn of first captain", () => {
+    const matchDetailsSmall = { actionsTakenOnMaps: 4, firstTeam: { captainId: "captainId" } };
+    const store = { state: { $user: { uid: "notCaptainId" } } };
+
+    expect(utils.canFirstCaptainTakeActionOnMap(matchDetailsSmall, store)).toBe(true);
+  });
+
+  test("canFirstCaptainTakeActionOnMap returns true if it is turn of first captain", () => {
+    const matchDetailsSmall = { actionsTakenOnMaps: 4, firstTeam: { captainId: "captainId" } };
+    const store = { state: { $user: { uid: "captainId" } } };
+
+    expect(utils.canFirstCaptainTakeActionOnMap(matchDetailsSmall, store)).toBe(false);
+  });
+
+  test("canSecondCaptainTakeActionOnMap returns true if it is turn of first captain", () => {
+    const matchDetailsSmall = { actionsTakenOnMaps: 5, secondTeam: { captainId: "captainId" } };
+    const store = { state: { $user: { uid: "notCaptainId" } } };
+
+    expect(utils.canSecondCaptainTakeActionOnMap(matchDetailsSmall, store)).toBe(true);
+  });
+
+  test("canSecondCaptainTakeActionOnMap returns true if it is turn of first captain", () => {
+    const matchDetailsSmall = { actionsTakenOnMaps: 5, secondTeam: { captainId: "captainId" } };
+    const store = { state: { $user: { uid: "captainId" } } };
+
+    expect(utils.canSecondCaptainTakeActionOnMap(matchDetailsSmall, store)).toBe(false);
+  });
+
+  const scores = [
+    { first_team_score: "16", second_team_score: "12" },
+    { first_team_score: "11", second_team_score: "16" },
+    { first_team_score: "19", second_team_score: "15" },
+    { first_team_score: "3", second_team_score: "16" },
+    { first_team_score: "19", second_team_score: "16" },
+  ];
+  test("getNumberOfMapsWonByFirstTeam counts number of maps won properly", () => {
+    const matchDetails = { scores: scores };
+    expect(utils.getNumberOfMapsWonByFirstTeam(matchDetails)).toBe(3);
+  });
+
+  test("getNumberOfMapsWonBySecondTeam counts number of maps won properly", () => {
+    const matchDetails = { scores: scores };
+    expect(utils.getNumberOfMapsWonBySecondTeam(matchDetails)).toBe(2);
+  });
+
+  const determineWinnerDetails = {
+    firstTeam: { name: "First team" },
+    secondTeam: { name: "Second team" },
+  };
+  test("determineWinner returns string with the name of the first team that won the BO1 match", () => {
+    determineWinnerDetails.matchType = types.MatchType.BO1;
+    determineWinnerDetails.firstTeamMapWins = 1;
+    determineWinnerDetails.secondTeamMapWins = 0;
+    expect(utils.determineWinner(determineWinnerDetails)).toBe("First team");
+  });
+
+  test("determineWinner returns string with the name of the second team that won the BO1 match", () => {
+    determineWinnerDetails.matchType = types.MatchType.BO1;
+    determineWinnerDetails.firstTeamMapWins = 0;
+    determineWinnerDetails.secondTeamMapWins = 1;
+    expect(utils.determineWinner(determineWinnerDetails)).toBe("Second team");
+  });
+
+  test("determineWinner returns empty string when none of the teams won the map in the BO1 match", () => {
+    determineWinnerDetails.matchType = types.MatchType.BO1;
+    determineWinnerDetails.firstTeamMapWins = 0;
+    determineWinnerDetails.secondTeamMapWins = 0;
+    expect(utils.determineWinner(determineWinnerDetails)).toBe("");
+  });
+
+  test("determineWinner returns string with the name of the first team that won the BO3 match", () => {
+    determineWinnerDetails.matchType = types.MatchType.BO3;
+    determineWinnerDetails.firstTeamMapWins = 2;
+    determineWinnerDetails.secondTeamMapWins = 1;
+    expect(utils.determineWinner(determineWinnerDetails)).toBe("First team");
+  });
+
+  test("determineWinner returns string with the name of the second team that won the BO3 match", () => {
+    determineWinnerDetails.matchType = types.MatchType.BO3;
+    determineWinnerDetails.firstTeamMapWins = 0;
+    determineWinnerDetails.secondTeamMapWins = 2;
+    expect(utils.determineWinner(determineWinnerDetails)).toBe("Second team");
+  });
+
+  test("determineWinner returns empty string when none of the teams won the map in the BO3 match", () => {
+    determineWinnerDetails.matchType = types.MatchType.BO3;
+    determineWinnerDetails.firstTeamMapWins = 0;
+    determineWinnerDetails.secondTeamMapWins = 0;
+    expect(utils.determineWinner(determineWinnerDetails)).toBe("");
+  });
+
+  test("determineWinner returns string with the name of the first team that won the BO5 match", () => {
+    determineWinnerDetails.matchType = types.MatchType.BO5;
+    determineWinnerDetails.firstTeamMapWins = 3;
+    determineWinnerDetails.secondTeamMapWins = 2;
+    expect(utils.determineWinner(determineWinnerDetails)).toBe("First team");
+  });
+
+  test("determineWinner returns string with the name of the second team that won the BO5 match", () => {
+    determineWinnerDetails.matchType = types.MatchType.BO5;
+    determineWinnerDetails.firstTeamMapWins = 1;
+    determineWinnerDetails.secondTeamMapWins = 3;
+    expect(utils.determineWinner(determineWinnerDetails)).toBe("Second team");
+  });
+
+  test("determineWinner returns empty string when none of the teams won the map in the BO5 match", () => {
+    determineWinnerDetails.matchType = types.MatchType.BO5;
+    determineWinnerDetails.firstTeamMapWins = 0;
+    determineWinnerDetails.secondTeamMapWins = 0;
+    expect(utils.determineWinner(determineWinnerDetails)).toBe("");
+  });
+
+  const firstTeam = {
+    matchesWon: 0,
+    roundsWon: 0,
+    matchesLost: 0,
+    roundsLost: 0,
+  };
+  const secondTeam = {
+    matchesWon: 0,
+    roundsWon: 0,
+    matchesLost: 0,
+    roundsLost: 0,
+  };
+
+  test("scoreTableComparator determines which team won more matches", () => {
+    const firstTeamWithMoreMatchesWon = Object.create(firstTeam);
+    firstTeamWithMoreMatchesWon.matchesWon = 1;
+    expect(utils.scoreTableComparator(firstTeamWithMoreMatchesWon, secondTeam)).toBe(-1);
+
+    const secondTeamWithMoreMatchesWon = Object.create(secondTeam);
+    secondTeamWithMoreMatchesWon.matchesWon = 1;
+    expect(utils.scoreTableComparator(firstTeam, secondTeamWithMoreMatchesWon)).toBe(1);
+  });
+
+  test("scoreTableComparator determines which team won more rounds", () => {
+    const firstTeamWithMoreRoundsWon = Object.create(firstTeam);
+    firstTeamWithMoreRoundsWon.roundsWon = 1;
+    expect(utils.scoreTableComparator(firstTeamWithMoreRoundsWon, secondTeam)).toBe(-1);
+
+    const secondTeamWithMoreRoundsWon = Object.create(secondTeam);
+    secondTeamWithMoreRoundsWon.roundsWon = 1;
+    expect(utils.scoreTableComparator(firstTeam, secondTeamWithMoreRoundsWon)).toBe(1);
+  });
+
+  test("scoreTableComparator determines which team lost less matches", () => {
+    const firstTeamWithLessMatchesLost = Object.create(firstTeam);
+    firstTeamWithLessMatchesLost.matchesLost = 1;
+    expect(utils.scoreTableComparator(firstTeamWithLessMatchesLost, secondTeam)).toBe(1);
+
+    const secondTeamWithLessMatchesLost = Object.create(secondTeam);
+    secondTeamWithLessMatchesLost.matchesLost = 1;
+    expect(utils.scoreTableComparator(firstTeam, secondTeamWithLessMatchesLost)).toBe(-1);
+  });
+
+  test("scoreTableComparator determines which team lost less rounds", () => {
+    const firstTeamWithLessRoundsLost = Object.create(firstTeam);
+    firstTeamWithLessRoundsLost.roundsLost = 1;
+    expect(utils.scoreTableComparator(firstTeamWithLessRoundsLost, secondTeam)).toBe(1);
+
+    const secondTeamWithLessRoundsLost = Object.create(secondTeam);
+    secondTeamWithLessRoundsLost.roundsLost = 1;
+    expect(utils.scoreTableComparator(firstTeam, secondTeamWithLessRoundsLost)).toBe(-1);
+  });
+
+  test("scoreTableComparator returns 0 when both team have the same statistics", () => {
+    expect(utils.scoreTableComparator(firstTeam, secondTeam)).toBe(0);
   });
 });
