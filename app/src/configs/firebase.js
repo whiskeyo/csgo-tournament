193dc601f116cad93f1aaa1e5d0546c2e783ea3b
whiskeyo <duchowe50k@gmail.com>
2022-06-23 01:47:34 +0200

    Change firebase config not to use api keys

--- app/src/configs/firebase.js
+++ app/src/configs/firebase.js
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
  * @object
  */
 const firebaseConfig = {
-  apiKey: "AIzaSyBXQMByC_McVaPW6FwuBiCOjK9H2iEzDjA",
-  authDomain: "cstrikers-dev-ff691.firebaseapp.com",
-  projectId: "cstrikers-dev-ff691",
-  storageBucket: "cstrikers-dev-ff691.appspot.com",
-  messagingSenderId: "328582704510",
-  appId: "1:328582704510:web:4c72e56e6881768c17572f",
-  measurementId: "G-KKYJW4NEL0",
+  apiKey: "PLACEHOLDER",
+  authDomain: "PLACEHOLDER",
+  projectId: "PLACEHOLDER",
+  storageBucket: "PLACEHOLDER",
+  messagingSenderId: "PLACEHOLDER",
+  appId: "PLACEHOLDER",
+  measurementId: "PLACEHOLDER",
 };
 
 export const app = initializeApp(firebaseConfig);
